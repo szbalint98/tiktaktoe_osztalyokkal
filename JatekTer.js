b6123c1fcaeee0b31d0d2c05ff4bb8fb157f57dc
baalintka <ballint3@gmail.com>
2024-09-20 15:04:04 +0200

    mezo kiir modositás

--- JatekTer.js
+++ JatekTer.js
@@ -1,15 +1,17 @@
 import Mezo from "./Mezo.js";
 export default class Jatekter{
     #szulElem
-    #lista= []
-    constructor(lista,szulElem){
+    
+    constructor(szulElem){
 
         this.szulelem=this.szulElem;
+        this.#mezoKiir();
         this.szulElem.empty();
     }
-    mezoKiir(){
-        this.#lista.forEach((element=>{
-            new Mezo(allapot);
-        }))
+    #mezoKiir(){
+        for (let index = 0; index < 9; index++) {
+                new Mezo(allapot);
+            
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import Jatekter from "./JatekTer.js";
 class Jatek{
     constructor(){
+        const jatek= new Jatekter()
 
-        
     }
 
 
